10987a4701f3d372f1ead30fa93a42f39f28e6ec
Silas Nordgren <nordgrensilas@gmail.com>
2019-11-12 11:44:42 +0100

    Add MySQL connectivity

--- epok-api/server.js
+++ epok-api/server.js
@@ -2,5 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: '178.62.11.13',
+  user: 'test',
+  password: 'testpass',
+  database: 'epok'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
 app.get('/', (req, res) => res.send("Hello, world!"));
 app.listen(port, () => console.log(`Listening on ${port}.`))
