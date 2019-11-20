c3fe566ed705ec404486cccb0c58683146730b70
Silas Nordgren <nordgrensilas@gmail.com>
2019-11-20 16:39:23 +0100

    Add support for storing grades in Ladok

--- ladok-api/server.js
+++ ladok-api/server.js
@@ -4,9 +4,59 @@ const app = express();
 const port = 3003;
 
 const axios = require('axios');
+// Ladda MySQL-biblioteket.
+const mysql = require('mysql');
 
+// Skapa en connection med vår DigitalOcean-host och användarnamn, lösenord, 
+// och epok-databasen.
+const connection = mysql.createConnection({
+  host: '178.62.11.13',
+  user: 'test',
+  password: 'testpass',
+  database: 'epok'
+});
+// Connecta till databasen.
+connection.connect();
+
+connection.query('drop table Grade;');
+connection.query(`
+create table if not exists Grade(
+  id int auto_increment primary key,
+  personId varchar(64) not null,
+  examId varchar(64) not null,
+  course varchar(64) not null,
+  grade varchar(64) not null
+);`)
+connection.query(`
+insert into Grade (personId, examId, course, grade) values ('Mattias', 'Tenta', 'Java 2', 'VG');`);
+
+// Läs in request-body som JSON.
 app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send("Hello, world! This is Martin"));
+
+app.get('/grades', (req, res) => {
+  const query = `
+select * from Grade;`;
+  connection.query(query, (error, results, fields) => {
+    if (error) throw error;
+    res.json(results);
+  })
+});
+
+app.get('/grade', (req, res) => {
+  const { personId, examId, course } = req.query;
+  const query = `
+select * from Grade where personId = '${personId}' 
+  and examId = '${examId}'
+  and course = '${course}';`;
+  console.log(query);
+  connection.query(query,
+    (error, results, fields) => {
+      if (error) throw error;
+      res.json(results);
+    });
+});
 
 /*
 {
@@ -21,17 +71,18 @@ app.post('/grade', (req, res) => {
   const personId = req.body.personId;
   const course = req.body.course;
   const examId = req.body.examId;
-
-  res.json(req.body);
+  res.json({});
 });
 
 app.listen(port, () => {
   console.log(`Listening on ${port}.`);
 
-  axios.post(`http://localhost:${port}/grade`, {
-    grade: '1',
-    personId: '2',
-    course: '3',
-    examId: '4'
+  axios.get(`http://localhost:${port}/grades`, {
+    params: {
+      // grade: '1',
+      personId: '1',
+      course: '4',
+      examId: '2'
+    }
   }).then(res => console.log(res.data));
 });
--- teacher-ui/server.js
+++ teacher-ui/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3004;
 
+const axios = require('axios');
+
 app.set('view engine', 'pug');
-app.get('/', (req, res) => res.render('index', {
-  title: 'hey',
-  message: 'Hello there',
-  grades: [
-    { name: 'A', grade: 'VG' }, 
-    { name: 'B', grade: 'MVG' }
-  ]
-}));
+app.get('/', (req, res) => {
+  const grades = axios.get('http://localhost:3003/grades').then(res => res.data).then(data => {
+    res.render('index', {
+      title: 'hey',
+      message: 'Hello there',
+      grades: data
+    });
+  });
+});
 app.listen(port, () => console.log(`Listening on ${port}.`))
