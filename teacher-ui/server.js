4e354609dc182ccc672aabc83904e5273afef556
Silas Nordgren <nordgrensilas@gmail.com>
2019-11-20 16:03:26 +0100

    Add table to Teacher UI

--- teacher-ui/server.js
+++ teacher-ui/server.js
@@ -5,6 +5,10 @@ const port = 3004;
 app.set('view engine', 'pug');
 app.get('/', (req, res) => res.render('index', {
   title: 'hey',
-  message: 'Hello there'
+  message: 'Hello there',
+  grades: [
+    { name: 'A', grade: 'VG' }, 
+    { name: 'B', grade: 'MVG' }
+  ]
 }));
 app.listen(port, () => console.log(`Listening on ${port}.`))
